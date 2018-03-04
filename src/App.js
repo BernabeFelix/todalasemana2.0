755c5d01afe30061623bc62b1f7833b16d96e05f
Bernabe <jesusbernabe2978@gmail.com>
2018-03-04 16:55:03 -0600

    Integrate react to webpack. Add webpack dev server

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+  return (
+    <div>
+      <p>React here! again!</p>
+    </div>
+  );
+};
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById("app"))
