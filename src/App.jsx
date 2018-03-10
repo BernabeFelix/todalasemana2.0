86843b5d57d5d8e467399cf487936ee723f1cd10
Bernabe <bernabe@maniak.com.mx>
2018-03-10 17:16:52 -0600

    Merge Sidevar with Header componnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import SideNav from './components/common/SideNav';
-import Main from './Main';
+import Main from './Content';
 import Header from './components/common/Header';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      sideNavOpen: false
-    };
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-  }
-
-  toggleDrawer() {
-    this.setState({
-      sideNavOpen: !this.state.sideNavOpen
-    });
-  }
-
-  render() {
-    return (
-      <MuiThemeProvider>
-        <Router>
-          <Fragment>
-            <Header onToggleDrawer={this.toggleDrawer} />
-            <SideNav
-              open={this.state.sideNavOpen}
-              onCloseDrawer={this.toggleDrawer}
-            />
-            <Main />
-          </Fragment>
-        </Router>
-      </MuiThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <MuiThemeProvider>
+    <Router>
+      <Fragment>
+        <Header />
+        <Main />
+      </Fragment>
+    </Router>
+  </MuiThemeProvider>
+);
 
 export default App;
-
-// ReactDOM.render(<App style={styles} />, document.getElementById('app'));
