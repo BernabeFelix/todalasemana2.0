ff5c6d94931c98d9987a49e63200363a494ef4cc
Bernabe <bernabe@maniak.com.mx>
2018-03-10 17:42:32 -0600

    Create routes file and Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <h1>Im Home</h1>;
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+export const aboutUrl = '/about';
+
+export const homeUrl = '/';
+
+export const dayUrl = '/day/:day';
