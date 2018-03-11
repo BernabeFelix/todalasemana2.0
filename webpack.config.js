6dd7400b3887ce32e58cfa94532e08b1ca68b120
Bernabe <bernabe@maniak.com.mx>
2018-03-11 17:15:27 -0600

    Remove relative paths

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: { useRelativePath: true }
+            options: {}
           }
         ]
       }
