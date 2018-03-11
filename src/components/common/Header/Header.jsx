d5853e2adc81619f3f086670f3b9f77afb6a175f
Paul Sanchez <polkien.sancer@gmail.com>
2018-03-11 17:15:49 -0600

    Day navigation in header for desktop

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import FlatButton from 'material-ui/FlatButton';
+// import FlatButton from 'material-ui/FlatButton';
 import { Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import SideNav from '../SideNav/SideNav';
@@ -21,12 +21,45 @@ class Header extends React.Component {
         <AppBar
           title="Toda la semana"
           className="header"
+          iconElementLeft={<img className="navbar-left-logo-img" alt="" />}
           onLeftIconButtonClick={this.toggleDrawer}
           iconElementRight={
-            <FlatButton
-              label="Martes"
-              containerElement={<Link to="signin" />}
-            />
+            <Fragment>
+              <div className="header-right-nav">
+                <Link to="/lunes" className="header-right-nav-btn">
+                  LUNES
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/martes" className="header-right-nav-btn">
+                  Martes
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/miercoles" className="header-right-nav-btn">
+                  Miercoles
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/jueves" className="header-right-nav-btn">
+                  Jueves
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/viernes" className="header-right-nav-btn">
+                  Viernes
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/sabado" className="header-right-nav-btn">
+                  Sabado
+                </Link>
+                <span className="header-right-nav-divider"> | </span>
+                <Link to="/domingo" className="header-right-nav-btn">
+                  Domingo
+                </Link>
+              </div>
+              <div className="header-right-nav">
+                <Link to="/entrar" className="header-right-nav-btn">
+                  Entrar
+                </Link>
+              </div>
+            </Fragment>
           }
         />
 
