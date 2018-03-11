f3737f7f1c8206022b1db042e7df05618dd71a98
Bernabe <bernabe@maniak.com.mx>
2018-03-11 16:08:31 -0600

    Improve routing. Improve typing. Replace hardcoded info by props. Add babel preset-2 to support spread operator.

--- src/components/Home/PromotionSingleResult.jsx
+++ src/components/Home/PromotionSingleResult.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { promotionUrl } from '../../routes';
+import { Promotion } from './types';
 
-const PromotionSingleResult = () => (
-  <div className="row promotion-single-result">
+const PromotionSingleResult = ({ description, id, imgUrl, title }) => (
+  <Link className="row promotion-single-result" to={promotionUrl({ id })}>
     <div className="col-xs-12 col-sm-3 img-container">
-      <img
-        className="img-responsive"
-        src="https://placebear.com/200/100"
-        alt=""
-      />
+      <img className="img-responsive" src={imgUrl} alt="" />
     </div>
     <div className="col-xs-12 col-sm-9">
-      <h3>Titulo</h3>
-      <p>Description</p>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
-  </div>
+  </Link>
 );
+
+PromotionSingleResult.propTypes = { ...Promotion };
 
 export default PromotionSingleResult;
--- src/components/Home/PromotionsResults.jsx
+++ src/components/Home/PromotionsResults.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { arrayOf, shape } from 'prop-types';
 import PromotionSingleResult from './PromotionSingleResult';
+import { Promotion } from './types';
 
-const PromotionsResults = () => <PromotionSingleResult />;
+const PromotionsResults = ({ promotions }) =>
+  promotions.map(promotion => (
+    <PromotionSingleResult key={promotion.id} {...promotion} />
+  ));
+
+PromotionsResults.defaultProps = {
+  promotions: [
+    {
+      id: 1,
+      description: 'Im the description',
+      imgUrl: 'https://placebear.com/200/100',
+      title: 'Im the title'
+    }
+  ]
+};
+
+PromotionsResults.propTypes = {
+  promotions: arrayOf(shape(Promotion))
+};
 
 export default PromotionsResults;
