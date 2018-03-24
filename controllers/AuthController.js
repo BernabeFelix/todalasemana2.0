19246e72389c93f20205a534a9467f3fcd52efe3
Polkien <polkien.sancer@gmail.com>
2018-03-24 16:06:39 -0600

    Improving express app structure and router definition

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,40 +6,40 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 // SignUp
-router.post('/', (req, res) => {
-  const { email, password } = req.body;
-  const ret = { user: { id: 0, email, password } };
-  // firebase create user
+router
+  .route('/')
+  .post('/', (req, res) => {
+    const { email, password } = req.body;
+    const ret = { user: { id: 0, email, password } };
+    // firebase create user
 
-  res.status(200).send(ret);
-});
-
-router.get('/', (req, res) => {
-  // firebase create user
-  res.status(200).send({
-    users: [
-      {
-        id: 0,
-        email: 'paul@hgmail.com',
-        password: '1234'
-      },
-      {
-        id: 2,
-        email: 'berna@hgmail.com',
-        password: '5678'
-      },
-      {
-        id: 3,
-        email: 'rosa@hgmail.com',
-        password: '4321'
-      },
-      {
-        id: 4,
-        email: 'ali@hgmail.com',
-        password: '8765'
-      }
-    ]
+    res.status(200).send(ret);
+  })
+  .get('/', (req, res) => {
+    res.status(200).send({
+      users: [
+        {
+          id: 0,
+          email: 'paul@hgmail.com',
+          password: '1234'
+        },
+        {
+          id: 2,
+          email: 'berna@hgmail.com',
+          password: '5678'
+        },
+        {
+          id: 3,
+          email: 'rosa@hgmail.com',
+          password: '4321'
+        },
+        {
+          id: 4,
+          email: 'ali@hgmail.com',
+          password: '8765'
+        }
+      ]
+    });
   });
-});
 
 export default router;
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import AuthController from './controllers/AuthController';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/api/auth', AuthController);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 export default app;
