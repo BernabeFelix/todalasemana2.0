3ddf908a3b8f96b9bb3c2d23729c82d0a6b75c97
Bernabe <bernabe@maniak.com.mx>
2018-03-30 16:01:46 -0600

    Add variables file for css-in-js styles. Style SnackBar

--- client/src/components/common/SnackBar/SnackBar.jsx
+++ client/src/components/common/SnackBar/SnackBar.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import Snackbar from 'material-ui/Snackbar';
 import { bool, func, number, string } from 'prop-types';
+import { $blueCool, $red, Intent } from '../../../styles/variables';
 
-const CustomSnackbar = ({ duration, handleRequestClose, msg, open }) => (
+const successStyle = {
+  backgroundColor: $blueCool
+};
+
+const errorStyle = {
+  backgroundColor: $red
+};
+
+const CustomSnackbar = ({
+  duration,
+  handleRequestClose,
+  msg,
+  open,
+  intent
+}) => (
   <Snackbar
     open={open}
     message={msg}
     autoHideDuration={duration}
     onRequestClose={handleRequestClose}
+    bodyStyle={intent === Intent.ERROR ? errorStyle : successStyle}
   />
 );
 
 CustomSnackbar.defaultProps = {
   duration: 2000,
+  intent: Intent.ERROR,
   open: false
 };
 
 CustomSnackbar.propTypes = {
   duration: number,
   handleRequestClose: func.isRequired,
+  intent: string,
   msg: string.isRequired,
   open: bool
 };
---
+++ client/src/styles/variables.js
@@ -0,0 +1,24 @@
+// NOTE: if you delete/add something in this file
+// make sure to do it also in variables.scss
+
+// variables
+
+export const $blueLighter = '#e5f5fb';
+export const $blueLight = '#b5e2f5';
+export const $blueMid = '#65c9d1';
+export const $blueDarks = '#5ba8d6';
+export const $blueCool = '#1f8f8f';
+export const $orange = '#f7941e';
+export const $red = '#ee3335';
+export const $white = '#ffffff';
+export const $black = '#000000';
+export const $grayBg = '#f2f2f2';
+
+// sizes
+export const $headerHeight = '64px';
+
+// intent
+export const Intent = {
+  ERROR: 'ERROR',
+  SUCCESS: 'SUCCESS'
+};
