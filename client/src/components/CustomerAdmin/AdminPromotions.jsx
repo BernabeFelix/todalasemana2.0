3840bcf04a4aaa6ea8da93b49c5b5da3b7c4d962
Bernabe <bernabe@maniak.com.mx>
2018-04-08 22:52:09 -0500

    Integrate apollo to AdminPromotions

--- client/src/components/CustomerAdmin/AdminPromotions.jsx
+++ client/src/components/CustomerAdmin/AdminPromotions.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { List } from 'material-ui';
-import { arrayOf, shape } from 'prop-types';
-import fakePromotions from '../../api/promotions';
+import { shape } from 'prop-types';
+import { gql } from 'apollo-boost/lib/index';
+import { Query } from 'react-apollo';
 import AdminPromotion from './AdminPromotion';
-import { History, Match, Promotion } from '../common/types';
+import { History, Match } from '../common/types';
 import { hasSlashAtTheEnd } from '../../utils/url';
+
+const query = gql`
+  {
+    promotions {
+      id
+    }
+  }
+`;
 
 class AdminPromotions extends Component {
   updateRoute = id => () => {
@@ -18,28 +27,32 @@ class AdminPromotions extends Component {
   };
 
   render() {
-    const { promotions } = this.props;
-    //  todo: create a 'no promotions message'
-    if (!promotions) return null;
-
     return (
-      <List style={{ backgroundColor: 'white', padding: 0 }}>
-        {promotions.map(({ id }) => (
-          <AdminPromotion id={id} onClick={this.updateRoute(id)} key={id} />
-        ))}
-      </List>
+      <Query query={query}>
+        {({ loading, data }) => {
+          //  todo: create a 'no promotions message'
+          if (loading) return null;
+
+          return (
+            <List style={{ backgroundColor: 'white', padding: 0 }}>
+              {data.promotions.map(({ id }) => (
+                <AdminPromotion
+                  id={id}
+                  onClick={this.updateRoute(id)}
+                  key={id}
+                />
+              ))}
+            </List>
+          );
+        }}
+      </Query>
     );
   }
 }
 
-AdminPromotions.defaultProps = {
-  promotions: fakePromotions
-};
-
 AdminPromotions.propTypes = {
   history: shape(History).isRequired,
-  match: shape(Match).isRequired,
-  promotions: arrayOf(shape(Promotion))
+  match: shape(Match).isRequired
 };
 
 export default AdminPromotions;
