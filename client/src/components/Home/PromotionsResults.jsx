e57a2cc8d0d7b80bd54193b0d3f56ea4cd642246
Bernabe <bernabe@maniak.com.mx>
2018-04-08 22:47:21 -0500

    Refactor get promotions to be use Query instead of HOC

--- client/src/components/Home/PromotionsResults.jsx
+++ client/src/components/Home/PromotionsResults.jsx
@@ -1,33 +1,9 @@
 import React from 'react';
-import { arrayOf, func, shape } from 'prop-types';
-import { graphql } from 'react-apollo';
+import { func } from 'prop-types';
 import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
 import { promotionUrl } from '../../routes';
 import PromotionSingleResult from './PromotionSingleResult';
-import { Promotion } from '../common/types';
-
-const PromotionsResults = ({ query, urlCallback }) =>
-  query.promotions
-    ? query.promotions.map(promotion => (
-        <PromotionSingleResult
-          key={promotion.id}
-          url={urlCallback}
-          {...promotion}
-        />
-      ))
-    : null;
-
-PromotionsResults.defaultProps = {
-  urlCallback: promotionUrl,
-  query: { promotions: [] }
-};
-
-PromotionsResults.propTypes = {
-  query: shape({
-    promotions: arrayOf(shape(Promotion))
-  }),
-  urlCallback: func
-};
 
 const query = gql`
   query {
@@ -41,4 +17,28 @@ const query = gql`
   }
 `;
 
-export default graphql(query, { name: 'query' })(PromotionsResults);
+const PromotionsResults = ({ urlCallback }) => (
+  <Query query={query}>
+    {({ loading, data }) => {
+      if (loading) return null;
+
+      return data.promotions.map(promotion => (
+        <PromotionSingleResult
+          key={promotion.id}
+          url={urlCallback}
+          {...promotion}
+        />
+      ));
+    }}
+  </Query>
+);
+
+PromotionsResults.defaultProps = {
+  urlCallback: promotionUrl
+};
+
+PromotionsResults.propTypes = {
+  urlCallback: func
+};
+
+export default PromotionsResults;
