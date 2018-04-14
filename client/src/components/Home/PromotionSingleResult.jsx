4289683b773cb5ea5b2df9801f2dbfad5bb30801
Polkien <polkien.sancer@gmail.com>
2018-04-14 14:50:12 -0500

    Share options for fb, tw and wa plus promo layout change

--- client/src/components/Home/PromotionSingleResult.jsx
+++ client/src/components/Home/PromotionSingleResult.jsx
@@ -1,25 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Paper } from 'material-ui';
 import { func } from 'prop-types';
+import { Paper } from 'material-ui';
+import Chip from 'material-ui/Chip';
+import ShareMenu from './ShareMenu';
+
 import { Promotion } from '../common/types';
 
+const styles = {
+  chip: {
+    float: 'right'
+  },
+  chipLabel: {
+    fontSize: '12px',
+    lineHeight: '24px'
+  }
+};
+
 const PromotionSingleResult = ({ description, id, imgUrl, url, title }) => (
-  <Link to={url({ id })}>
-    <Paper className="promotion-single-result" zDepth={2}>
-      <div className="row">
-        <div className="col-xs-12 col-sm-3">
+  <Paper className="promotion-single-result" zDepth={2}>
+    <div className="row">
+      {/* <div className="col-xs-12 col-sm-3">
+        <Link to={url({ id })}>
           <div className="img-container">
             <img className="img-responsive" src={imgUrl} alt="" />
           </div>
+        </Link>
+      </div> */}
+      <div className="col-xs-12 col-sm-12">
+        <h4 className="company">
+          Pizza Hut &copy; <span className="time" />
+          <ShareMenu title={title} url="https://google.com" />
+        </h4>
+
+        <div className="row">
+          <div className="col-xs-12">
+            <Link to={url({ id })}>
+              <div className="img-container">
+                <img className="img-responsive" src={imgUrl} alt="" />
+              </div>
+            </Link>
+          </div>
         </div>
-        <div className="col-xs-12 col-sm-9">
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+        <h4 className="title">
+          {title}
+          <Chip style={styles.chip} labelStyle={styles.chipLabel}>
+            Comida
+          </Chip>
+        </h4>
+        <small className="availability">Expira el 12/05/2018</small>
+        <p>{description}</p>
       </div>
-    </Paper>
-  </Link>
+    </div>
+  </Paper>
 );
 
 PromotionSingleResult.propTypes = {
