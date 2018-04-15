2d83c909014adb424d0ccccf058b86f13865b1c7
Polkien <polkien.sancer@gmail.com>
2018-04-14 19:36:32 -0500

    Improved the way to obtain root url

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,3 +1,5 @@
-export const hasSlashAtTheEnd = str => {
-  return str.lastIndexOf('/') === str.length - 1;
-};
+import settings from '../../settings.json';
+
+export const hasSlashAtTheEnd = str => str.lastIndexOf('/') === str.length - 1;
+
+export const getRootUrl = () => settings.rootUrl;
