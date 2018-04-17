0542a98f74056a9e6687c708d3014af078263f24
Bernabe <bernabe@maniak.com.mx>
2018-04-17 01:42:05 -0500

    Add js to babel in webpack

--- client/src/utils/requiredIf.js
+++ client/src/utils/requiredIf.js
@@ -1,18 +1,18 @@
-const requiredIf = (type, condition) => {
-  return function(props, propName, componentName) {
+const requiredIf = (type, condition) =>
+  function(props, propName, componentName) {
     if (typeof type !== 'function') {
       return new Error(
-        'Invalid react-required-if prop type supplied to ' +
-          componentName +
-          '. Validation failed.'
+        `Invalid react-required-if prop type supplied to 
+          ${componentName}.
+          Validation failed.`
       );
     }
 
     if (typeof condition !== 'function') {
       return new Error(
-        'Invalid react-required-if condition supplied to ' +
-          componentName +
-          '. Validation failed.'
+        `Invalid react-required-if condition supplied to 
+          ${componentName}.
+          Validation failed.`
       );
     }
 
@@ -20,6 +20,5 @@ const requiredIf = (type, condition) => {
 
     return test.apply(this, arguments);
   };
-};
 
 export default requiredIf;
