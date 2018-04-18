9d4fb40dea7f0fbcbc3b48c3c6bb0348ba911e09
Bernabe <bernabe@maniak.com.mx>
2018-04-17 22:47:46 -0500

    Delete all backend

--- routes/clients/all.js
+++
@@ -1,5 +0,0 @@
-import clients from './clients';
-
-const all = ({ res }) => res.status(200).json({ clients });
-
-export default all;
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
-import expressValidator from 'express-validator';
-import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// This is needed for apollo in the frontend
-app.use(cors());
+// @paul are these needed?
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(expressValidator());
 
 // API
-
-app.use('/api', routes);
-
 app.use('/public', express.static('public', { root: __dirname }));
 
 // Default HTML
