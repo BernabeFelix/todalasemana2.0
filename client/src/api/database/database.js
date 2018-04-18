0ba834d2e18ed863c1182511dca896966701f8d2
Bernabe <bernabe@maniak.com.mx>
2018-04-17 21:15:34 -0500

    Improve database class

--- client/src/api/database/database.js
+++ client/src/api/database/database.js
@@ -4,13 +4,13 @@ class DataBase {
   static database = FirebaseApp.database();
 
   static async add(ref, data) {
-    const newPromotion = this.database.ref(ref).push();
+    const newPromotion = DataBase.database.ref(ref).push();
 
     return await newPromotion.set(data);
   }
 
   static update = async (ref, data) =>
-    await this.database.ref(ref).update(data);
+    await DataBase.database.ref(ref).update(data);
 
   static get = async (ref, onSuccess) => await DataBase.database.ref(ref).once('value', onSuccess);
 }
